a3999913893b9c681a0d61206c866419bc8e31ae
achielikun <achiel.chouchane@gmail.com>
2026-02-21 20:15:39 +0100

    added coin-adt and updated draw-adt

--- draw-adt.rkt
+++ draw-adt.rkt
@@ -15,22 +15,33 @@
       (let ((window (make-window width  height "pac-man phase 1")))
         ((window 'set-background!) "black")
 
-      (define pac-man-layer ((window 'new-layer!)))
-      (define pac-man-tile #|image|#) 
-
-
-
-      (define level-layer ((window 'new-layer!)))
-      (define level-tile #|bitmap|# )
-
-
-      (define coin-layer ((window 'new-layer!)))
-      (define coin-tile #|image|#)
-
- 
-      
-
-      (lambda (msg)
-        (cond ((eq? msg 'start-draw!) )
-              (else (error "draw adt -- unknown message:" msg))))))))
+        (define (draw-object! obj tile)
+          (let* ((pos (obj 'position))
+                 (x (pos 'x))
+                 (y (pos 'y))
+                 (pos-x (* x cel-width-px))
+                 (pos-y (* y cel-height-px)))
+            ((tile 'set-x!) pos-x)
+            ((tile 'set-y!) pos-y)))
+        
+        
           
+        
+        (define pac-man-layer ((window 'new-layer!)))
+        (define pac-man-tile #|image|#) 
+        
+        
+        
+        (define level-layer ((window 'new-layer!)))
+        (define level-tile #|bitmap|# )
+        
+        
+        (define coin-layer ((window 'new-layer!)))
+        (define coin-tile #|image|#)
+        
+        
+        
+        
+        (lambda (msg)
+          (cond ((eq? msg 'start-draw!) )
+                (else (error "draw adt -- unknown message:" msg))))))))
